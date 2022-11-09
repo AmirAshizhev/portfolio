1d36313194806d14930b0cd8b8eba844fa3867ce
Amir Ashizhev <amirashizhev@yandex.ru>
2022-11-09 20:02:38 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>HELLO WORLD</p>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
