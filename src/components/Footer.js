1b70184c8166a23ceb897ac54e43fe060c96c199
Amir Ashizhev <amirashizhev@yandex.ru>
2023-01-28 14:27:54 +0300

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,17 @@ function Footer() {
   return (
     <footer className='footer'>
       <div className='footer__box'>
-        <p>&#169; Амир Ашижев 2023</p>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <p className='footer__text'>&#169; Амир Ашижев 2023</p>
+        <ul className='footer__list'>
+          <li className='footer__item'>
+            <a className='footer__link' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'>LinkedIn</a>
+          </li>
+          <li className='footer__item'>
+            <a className='footer__link' rel="noreferrer noopener" href='https://t.me/Embraze11' target='_blank'>Telegram</a>
+          </li>
+          <li className='footer__item'>
+            <a className='footer__link' rel="noreferrer noopener" href='mailto:amirashizhev@yandex.ru'>Email</a>
+          </li>
         </ul>
       </div>
     </footer>
