fce6d2c7dc9d848e1d8022cb6026454389e9350b
Amir Ashizhev <amirashizhev@yandex.ru>
2023-04-22 20:09:02 +0300

    start projects page: progect__list card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,11 @@
+import noneImage from '../images/noneImage.svg'
+
 function ProjectCard({ name, stack }) {
   return (
     <li className='progect-card'>
-      <img src='' alt='' className='progect-card__img'/>
+      <img src={noneImage} alt='' className='progect-card__img'/>
       <div className='progect-card__info'>
-        <h4 className='progect-card__title'>{`Название: ${name}`}</h4>
+        <h4 className='progect-card__title'>{name}</h4>
         <p className='progect-card__about'>{`Стек технологий: ${stack}`}</p>   
       </div>
 
