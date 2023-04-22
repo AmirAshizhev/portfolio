626123746c02d78b949bb7f494cf0483c1a6e717
Amir Ashizhev <amirashizhev@yandex.ru>
2023-04-22 21:43:03 +0300

    fixing portfolio margin

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,10 @@
 function About(){
   return (
-    <h1>Скоро появитися инфа обо мне</h1>
+    <>
+      <h1>Скоро появитися инфа обо мне</h1>
+      <a href='https://disk.yandex.ru/i/2zTktHFDcHeNaQ' target='_blank' rel="noreferrer" >Резюме</a>
+    </>
+
   )
 }
 
