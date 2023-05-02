ffabda9230a553a145c5b65c3f64d86ba902cced
Amir Ashizhev <amirashizhev@yandex.ru>
2023-05-02 05:55:01 +0300

    fixes: footer content: removed links

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,7 +3,7 @@ function Contacts() {
     <section className="contacts">
       <div className="contacts__box">
         <div>
-          <h3>Написать мне:</h3>
+          <h3 className="contacts__title">Написать мне:</h3>
           <ul className="texting__list">
             <li className="texting__item texting__item_contact_email">amirashizhev@yandex.ru</li> 
             <li className="texting__item texting__item_contact_tg">@Embraze11</li>
@@ -11,10 +11,10 @@ function Contacts() {
           </ul> 
         </div>
         <div>
-          <h3>Медиа:</h3> 
+          <h3 className="contacts__title">Медиа:</h3> 
           <ul className="media__list">
-            <li><a className='media__link media__link_site_github' rel="noreferrer noopener" href='https://github.com/AmirAshizhev' target='_blank'></a></li> 
-            <li><a className='media__link media__link_site_linkedin' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'></a></li>
+            <li><a className='media__link media__link_site_github' rel="noreferrer noopener" href='https://github.com/AmirAshizhev' target='_blank'> </a></li> 
+            <li><a className='media__link media__link_site_linkedin' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'> </a></li>
           </ul> 
         </div> 
       </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     <footer className='footer'>
       <div className='footer__box'>
         <p className='footer__text'>&#169; Амир Ашижев 2023</p>
-        <ul className='footer__list'>
+        {/* <ul className='footer__list'>
           <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'>LinkedIn</a>
           </li>
@@ -13,7 +13,7 @@ function Footer() {
           <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='mailto:amirashizhev@yandex.ru'>Email</a>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </footer>
   )
