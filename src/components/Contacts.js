a357dee0e96c2b6a5c7638d1d2366f54cdcb302f
Amir Ashizhev <amirashizhev@yandex.ru>
2023-05-04 21:24:51 +0300

    fixes for footer

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,21 +2,14 @@ function Contacts() {
   return(
     <section className="contacts">
       <div className="contacts__box">
-        <div>
+
           <h3 className="contacts__title">Написать мне:</h3>
           <ul className="texting__list">
             <li className="texting__item texting__item_contact_email">amirashizhev@yandex.ru</li> 
             <li className="texting__item texting__item_contact_tg">@Embraze11</li>
             <li className="texting__item texting__item_contact_phone">8(904)337-33-92</li> 
           </ul> 
-        </div>
-        <div>
-          <h3 className="contacts__title">Медиа:</h3> 
-          <ul className="media__list">
-            <li><a className='media__link media__link_site_github' rel="noreferrer noopener" href='https://github.com/AmirAshizhev' target='_blank'> </a></li> 
-            <li><a className='media__link media__link_site_linkedin' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'> </a></li>
-          </ul> 
-        </div> 
+
       </div>
     </section> 
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,23 @@ function Footer() {
     <footer className='footer'>
       <div className='footer__box'>
         <p className='footer__text'>&#169; Амир Ашижев 2023</p>
-        {/* <ul className='footer__list'>
+        <ul className='footer__list'>
           <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'>LinkedIn</a>
           </li>
-          <li className='footer__item'>
+          {/* <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='https://t.me/Embraze11' target='_blank'>Telegram</a>
           </li>
           <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='mailto:amirashizhev@yandex.ru'>Email</a>
+          </li> */}
+          <li className='footer__item'>
+            <a className='footer__link' rel="noreferrer noopener" href='https://github.com/AmirAshizhev' target='_blank'>Github</a>
           </li>
-        </ul> */}
+          {/* <li className='footer__item'>
+            <a className='footer__link' rel="noreferrer noopener" href='mailto:amirashizhev@yandex.ru'>Email</a>
+          </li> */}
+        </ul>
       </div>
     </footer>
   )
