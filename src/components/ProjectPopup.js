3a5474bd608f95a02567df82e5cfdb36fbc4e635
Amir Ashizhev <amirashizhev@yandex.ru>
2023-05-06 17:12:53 +0300

    feat: popups images and ann more data

--- src/components/ProjectPopup.js
+++ src/components/ProjectPopup.js
@@ -3,13 +3,13 @@ function ProjectPopup ({isOpen, project, handleClosePopup}){
     <div className={`popup ${isOpen && 'popup_opened'}`}>
       <div className="popup__container">
         <button className="popup__btn" type="button" area-label="Закрыть" onClick={handleClosePopup}></button>
-        <img src="" alt="" className="popup__img"/>
+        <img src={project?.image} alt={project?.name} className="popup__img"/>
         <div className="popup__about">
           <p className="popup__title">Название: <span className="popup__span">{project?.name}</span></p>
-          <p className="popup__title">Стек технологий: <span className="popup__span">{project?.name}</span></p>
-          <p className="popup__title">Ссылка на гитхаб: <span className="popup__span">{project?.name}</span></p>
-          <p className="popup__title">Ссылка на саму работу: <span className="popup__span">{project?.name}</span></p>
-          <p className="popup__title">Описание: <span className="popup__span">{project?.name}</span></p>
+          <p className="popup__title">Стек технологий: <span className="popup__span">{project?.stack}</span></p>
+          <p className="popup__title">Ссылка на гитхаб: <span className="popup__span">{project?.ghLink}</span></p>
+          <p className="popup__title">Ссылка на саму работу: <span className="popup__span">{project?.link}</span></p>
+          <p className="popup__title">Описание: <span className="popup__span">{project?.about}</span></p>
         </div>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,37 +1,46 @@
+import previewHachaton from './images/preview_hackaton.png' 
+import previewHotel from './images/preview_hotel.png' 
+import previewRissian from './images/preview_russian.png' 
+
 export const projectData = [
   {
-    name: 'Место', 
-    stack: 'основной стек', 
-    ghLink: 'ссылка на гитхаб', 
-    link: 'ссылка на проект', 
-    about: 'описание'
-  },
-  {
-    name: 'Путешествие по России', 
-    stack: 'основной стек', 
-    ghLink: 'ссылка на гитхаб', 
-    link: 'ссылка на проект', 
-    about: 'описание'
+    name: 'Хакатон Яндекс', 
+    stack: 'HTML, CSS, JS, SCSS, webpack', 
+    ghLink: 'https://github.com/IgorTimonin/YandexHackathon', 
+    link: 'https://yandex-hackathon.vercel.app/', 
+    about: 'Проект, выполенный в рамках хакатона, - HR landing page, на которой рассказывается о вакансиях экспертов сопровождения и работе в Яндекс Практикуме. Эксперты сопровождения - это наставники и ревьюеры, работающие парт-тайм на обучающих курсах Практикума.',
+    image: previewHachaton,
   },
   {
     name: 'Поисковик отелей', 
-    stack: 'основной стек', 
-    ghLink: 'ссылка на гитхаб', 
-    link: 'ссылка на проект', 
-    about: 'описание'
+    stack: 'React.js, Redux, JavaScript, HTML5, CSS3', 
+    ghLink: 'https://github.com/AmirAshizhev/simple-hotel-check', 
+    link: 'https://amirashizhev.github.io/simple-hotel-check/', 
+    about: 'описание',
+    image: previewHotel,
   },
   {
-    name: 'Хакатон Яндекс', 
-    stack: 'основной стек', 
-    ghLink: 'ссылка на гитхаб', 
-    link: 'ссылка на проект', 
-    about: 'описание'
+    name: 'Место', 
+    stack: 'React.js, JavaScript, HTML5, CSS3', 
+    ghLink: 'https://github.com/AmirAshizhev/react-mesto-auth', 
+    link: 'https://amirashizhev.github.io/react-mesto-auth/', 
+    about: 'Учебный проект в рамках курса Яндекс.Практикума Веб-разработчик. Он представляет собой страничку, где можно размещать свои карточки, а так же смореть на карточки размещенные другими людьми. Присутствует возможность возможность регистрации пользователя, а также входа в систему зарегестрированному пользователю.',
+    image: '',
   },
   {
     name: 'Поисковик фильмов', 
-    stack: 'основной стек', 
-    ghLink: 'ссылка на гитхаб', 
-    link: 'ссылка на проект', 
-    about: 'описание'
-  }
+    stack: 'React.js, JavaScript, HTML5, CSS3, Node.js', 
+    ghLink: 'https://github.com/AmirAshizhev/movies-explorer-frontend', 
+    link: 'проект пока не размещен', 
+    about: 'Дипломный проект в рамках завершения Яндекс.Практикума Веб-разработчик. ',
+    image: '',
+  },
+  {
+    name: 'Путешествие по России', 
+    stack: 'HTML5, CSS3', 
+    ghLink: 'https://github.com/AmirAshizhev/russian-travel', 
+    link: 'https://amirashizhev.github.io/russian-travel/', 
+    about: 'Учебный проект в рамках курса Яндекс.Практикума Веб-разработчик. Он представляет собой адаптивный под разные экраны лендинг с картинками и ссылками на статьи и ресурсы о путешествиях по России.',
+    image: previewRissian,
+  },
 ];
