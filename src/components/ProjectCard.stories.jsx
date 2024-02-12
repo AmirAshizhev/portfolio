fa22eebb0e42b82e616bce1a29347d818c95c2f8
Amir Ashizhev <amirashizhev@yandex.ru>
2024-02-12 21:59:22 +0300

    feat: поправил внешний вид попапа карточки для десктоп версии

---
+++ src/components/ProjectCard.stories.jsx
@@ -0,0 +1,9 @@
+import ProjectCard from './ProjectCard';
+import '../index.css';
+
+export default {
+  title: 'ProjectCard',
+  component: ProjectCard,
+}
+
+export const Default = () => <ProjectCard/>
