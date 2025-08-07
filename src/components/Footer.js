f1927a29434058995ec7f074fe8c0bbe4baa572b
Amir Ashizhev <amirashizhev@yandex.ru>
2025-08-07 21:47:45 +0300

    lil fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className='footer'>
       <div className='footer__box'>
-        <p className='footer__text'>&#169; Амир Ашижев 2023</p>
+        <p className='footer__text'>&#169; Амир Ашижев 2025</p>
         <ul className='footer__list'>
           <li className='footer__item'>
             <a className='footer__link' rel="noreferrer noopener" href='https://www.linkedin.com/in/amirashizhev/' target='_blank'>LinkedIn</a>
@@ -14,6 +14,6 @@ function Footer() {
       </div>
     </footer>
   )
-};
+}
 
 export default Footer;
